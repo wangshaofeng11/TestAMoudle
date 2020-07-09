c2353429757e906462d6384d37f49020b8f5f654
wangshaofeng <wangshaofeng@xdf.cn>
2020-07-09 16:53:31 +0800

    增加文件B

---
+++ TestAMoudle/Classes/BppViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BppViewController.swift
+//  TestAMoudle
+//
+//  Created by wsf on 2020/7/9.
+//
+
+import UIKit
+
+class BppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
