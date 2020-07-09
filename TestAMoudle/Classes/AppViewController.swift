d8080f9206eda59bcd2fd16f0cccada2684cd550
wangshaofeng <wangshaofeng@xdf.cn>
2020-07-09 16:36:56 +0800

    模块A

---
+++ TestAMoudle/Classes/AppViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AppViewController.swift
+//  koolearn_toefl
+//
+//  Created by Mrlu on 2018/9/6.
+//  Copyright © 2018 koolearn. All rights reserved.
+//
+
+import UIKit
+
+public class AppViewController:UIViewController{
+    
+}
